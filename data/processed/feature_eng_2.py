17024829b09c9410d1de806c73e80bf73eca11a2
John Soychak <jsoych@users.noreply.github.com>
2024-06-20 18:16:27 -0400

    Added another pair of training and test data to investigate. I used another pretrained model (Xception) to process our raw data. Unfortunately, I had to zip the files together (data/processed/data_2.zip).

--- data/processed/feature_eng_2.py
+++ data/processed/feature_eng_2.py
@@ -13,21 +13,25 @@ test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
-vgg19 = tf.keras.applications.VGG19(
+model = tf.keras.applications.Xception(
     include_top=False,
     pooling="avg"
 )
 
-with open("data/processed/vgg19_train_data.csv", "w", newline="") as csvfile:
+preprocess_input = tf.keras.applications.xception.preprocess_input
+
+with open("data/processed/train_data_2.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     for image_batch, label_batch in iter(train_dataset):
-        feature_batch = vgg19(image_batch)
-        for i in range(batch_size):
+        image_batch = preprocess_input(image_batch)
+        feature_batch = model(image_batch)
+        for i in range(min(batch_size,image_batch.shape[0])):
             writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
 
-with open("data/processed/vgg19_test_data.csv", "w", newline="") as csvfile:
+with open("data/processed/test_data_2.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     for image_batch, label_batch in iter(test_dataset):
-        feature_batch = vgg19(image_batch)
-        for i in range(batch_size):
+        image_batch = preprocess_input(image_batch)
+        feature_batch = model(image_batch)
+        for i in range(min(batch_size,image_batch.shape[0])):
             writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
