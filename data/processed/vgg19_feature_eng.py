5f5e2d8b0e5cbc19e60599ef16a7c9ae3751c885
John Soychak <jsoych@users.noreply.github.com>
2024-06-20 09:28:05 -0400

    Added processsed test data to data/processed as vgg19_test_data.csv

--- data/processed/vgg19_feature_eng.py
+++ data/processed/vgg19_feature_eng.py
@@ -2,8 +2,15 @@ import csv
 import tensorflow as tf
 
 
+batch_size = 32
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    "data/raw/chest_xray/train"
+    "data/raw/chest_xray/train",
+    batch_size=batch_size
+)
+
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "data/raw/chest_xray/test",
+    batch_size=batch_size
 )
 
 vgg19 = tf.keras.applications.VGG19(
@@ -11,10 +18,16 @@ vgg19 = tf.keras.applications.VGG19(
     pooling="avg"
 )
 
-csvfile = open("data/processed/vgg19_train_data.csv", "w", newline="")
-writer = csv.writer(csvfile)
-for image_batch, label_batch in iter(train_dataset):
-    feature_batch = vgg19(image_batch)
-    for i in range(32):
-        writer.writerow([*list(feature_batch[i].numpy()),label_batch[i].numpy()])
-csvfile.close()
+with open("data/processed/vgg19_train_data.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    for image_batch, label_batch in iter(train_dataset):
+        feature_batch = vgg19(image_batch)
+        for i in range(batch_size):
+            writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
+
+with open("data/processed/vgg19_test_data.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    for image_batch, label_batch in iter(test_dataset):
+        feature_batch = vgg19(image_batch)
+        for i in range(batch_size):
+            writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
