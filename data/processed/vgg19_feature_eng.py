92edc91aabf9e8cd36d27137498cf3ffafaede82
John Soychak <jsoych@users.noreply.github.com>
2024-06-20 16:17:53 -0400

    Modified vgg19_feature_eng.py so that each batch is preprocessed before calling vgg19, and fixed an indexing problem (the last batch_size is not 32, since our data is not a multiple of 32). After making such changes, I ran the script again, and updated our training, and test data.

--- data/processed/vgg19_feature_eng.py
+++ data/processed/vgg19_feature_eng.py
@@ -18,16 +18,20 @@ vgg19 = tf.keras.applications.VGG19(
     pooling="avg"
 )
 
+preprocess_input = tf.keras.applications.vgg19.preprocess_input
+
 with open("data/processed/vgg19_train_data.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     for image_batch, label_batch in iter(train_dataset):
+        image_batch = preprocess_input(image_batch)
         feature_batch = vgg19(image_batch)
-        for i in range(batch_size):
+        for i in range(min(batch_size,image_batch.shape[0])):
             writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
 
 with open("data/processed/vgg19_test_data.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     for image_batch, label_batch in iter(test_dataset):
+        image_batch = preprocess_input(image_batch)
         feature_batch = vgg19(image_batch)
-        for i in range(batch_size):
+        for i in range(min(batch_size,image_batch.shape[0])):
             writer.writerow([*feature_batch[i].numpy(),label_batch[i].numpy()])
